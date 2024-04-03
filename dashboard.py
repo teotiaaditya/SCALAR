b8ec94be47f49db19e8548f991b678cd017c28be
aditya teotia <teotiaaditya.170302@gmail.com>
2024-04-03 13:46:47 +0530

    first commit

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,6 @@
 from tkinter import*
 from PIL import Image, ImageTk #pip install pillow py
+from course import CourseClass
 
 class RMS:
     def __init__(self,root):
@@ -15,7 +16,7 @@ class RMS:
         M_Frame=LabelFrame(self.root, text="Menus", font=( "times new roman" ,15),bg="white")
         M_Frame.place(x=10,y=70,width=1340, height=80)
 
-        btn_course=Button(M_Frame, text="Course", font=("goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2").place(x=20,y=5,width=200,height=46)
+        btn_course=Button(M_Frame, text="Course", font=("goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2",command=self.add_course).place(x=20,y=5,width=200,height=46)
         btn_course=Button(M_Frame, text="View Result", font=("goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2").place(x=240,y=5,width=200,height=46)
         btn_course=Button(M_Frame, text="Course", font=("goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2").place(x=460,y=5,width=200,height=46)
         
@@ -26,15 +27,20 @@ class RMS:
 
         self.lbl_bg=Label(self.root,image=self.bg_img).place(x=400,y=180,width=920,height=350)
 
+        #===update detail=====
         self.lbl_course=Label(self.root,text="Total Courses\n[ 0 ]",font=("goudy old style",20),bd=10,relief=RIDGE,bg="#e43b06",fg="white")
         self.lbl_course.place(x=400,y=530,width=300, height=100)
         self.lbl_student=Label(self.root,text="Total Students\n[ 0 ]",font=("goudy old style",20),bd=10,relief=RIDGE,bg="#0676ad",fg="white")
         self.lbl_student.place(x=710,y=530,width=300, height=100)
         self.lbl_result=Label(self.root,text="Total Results\n[ 0 ]",font=("goudy old style",20),bd=10,relief=RIDGE,bg="#038074",fg="white")
         self.lbl_result.place(x=1020,y=530,width=300, height=100)
-
+    
+        #===footer=====
         footer=Label(self.root, text="Student Result Managment System\nContact us for any info 999xxxxx11.", font=("goudy old style", 15), bg="#262626", fg="white") .pack(side=BOTTOM, fill=X)
-        
+    
+    def add_course (self) :
+        self.new_win=Toplevel(self.root)
+        self.new_obj=CourseClass(self.new_win)
 
 if __name__=="__main__":
     root=Tk()
